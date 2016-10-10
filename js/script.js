91f8de508e48fbe95095737d2cc8c5f61ba0f863
MewtoPewto <ashishthakursemail@gmail.com>
2016-10-09 17:17:09 -0700

    Added random youtube links!

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
 // Beginning of script
 console.log('script.js has been loaded.');
 
-// Begin Firebase magic
+// Begin link magic
+let youtubeArray = [
+  'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+  'https://www.youtube.com/watch?v=7bCzbUiB87M',
+  'https://www.youtube.com/watch?v=vfc42Pb5RA8',
+  'https://www.youtube.com/watch?v=G9s-6Ce25Ps',
+]
+
+function randomizer(bottom, top) {
+  return Math.floor( Math.random() * ( 1 + top - bottom ) ) + bottom;
+}
+
+function setLink() {
+  let button = document.getElementById('video-link-button');
+  let randomNumber = randomizer(0, youtubeArray.length - 1);
+
+  let videoLink = youtubeArray[randomNumber];
+
+  button.setAttribute('href', videoLink)
+}
+
+setLink();
